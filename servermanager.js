96d9b72b5749efe8d11c7b95897a695d92e68b23
Jeffrey <39491065+LeJeffy@users.noreply.github.com>
2019-01-29 16:10:48 +0100

    Cleaned some things up.

--- servermanager.js
+++ servermanager.js
@@ -1,36 +1,26 @@
-const Discovery = require("node-gtacoop-discovery")
-const request   = require("request")
+const Discovery = require("node-gtacoop-discovery");
+const request   = require("request");
+let servers = [];
 
-let servers = []
-
-function reloadServers(){
-	servers.length = 0
-	
-	request({ url: "https://master.gtacoop.com/", json: true }, handleMasterResponse)
+function reloadServers() {
+	servers.length = 0;
+	request({ url: 'https://master.gtacoop.com/', json: true }, handleMasterResponse);
 }
 
 function handleMasterResponse(error, response, body){
-	if(error) return;
-	
-	for(var address in body.list){
+	if (error) return;
+	for (var address in body.list) {
 		const [ ip, port ] = body.list[address].split(":");
-			
-		new Discovery(ip, port).send()
-		.then(response => {
+		new Discovery(ip, port).send().then(response => {
 			response.address = `${ip}:${port}`;
-			
-			// push to serverlist
-			servers.push(response)
-		})
-		// we want to catch errors but dont really do anything with them
-		.catch(x => {})
+			servers.push(response); // push to serverlist
+		}).catch(x => {}); // we want to catch errors but dont really do anything with them
 	}
 }
 
-module.exports.start = function(){
-	reloadServers()
-	
-	setInterval(reloadServers, 60 * 1000)
+module.exports.start = function() {
+	reloadServers();
+	setInterval(reloadServers, 60 * 1000);
 }
 
-module.exports.servers = servers
+module.exports.servers = servers;
